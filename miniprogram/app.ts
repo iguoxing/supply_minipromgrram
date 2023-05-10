5f964e3267cc218fa3db61e0e03331d074ab8010
ardenzhao <iguoxing@163.com>
2023-05-10 18:07:38 +0800

    feat: get token

--- miniprogram/app.ts
+++ miniprogram/app.ts
@@ -10,8 +10,31 @@ App<IAppOption>({
     // 登录
     wx.login({
       success: res => {
-        console.log(res.code)
+        console.log('code---',res.code)
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        if(!wx.getStorageSync("token")){
+          wx.request({
+            url: 'http://localhost:3000/v1/token', //仅为示例，并非真实的接口地址
+            method: 'POST',
+            data: {
+              "type":100,
+              "account":res.code
+            },
+            header: {
+              'content-type': 'application/json' // 默认值
+            },
+            success (res) {
+              console.log(res.data.token)
+              wx.setStorageSync("token",res && res.data && res.data.token)
+              wx.showToast({
+                title: '登录成功',
+                icon: 'success',
+                duration: 2000
+              })
+              
+            }
+          })
+        }
       },
     })
   },
--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -27,6 +27,16 @@ Page({
         canIUseGetUserProfile: true
       })
     }
+    // wx.request({
+    //   url: 'http://localhost:3000/v1/user/get', //仅为示例，并非真实的接口地址
+    //   data: {},
+    //   header: {
+    //     'content-type': 'application/json' // 默认值
+    //   },
+    //   success (res) {
+    //     console.log(res.data)
+    //   }
+    // })
   },
   getUserProfile() {
     // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
